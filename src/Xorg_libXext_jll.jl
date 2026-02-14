5e71f7208cae3efea16b2809290e830785012491
jlbuild <juliabuildbot@gmail.com>
2026-02-14 19:48:17 +0000

    Xorg_libXext_jll build 1.3.8+0

--- src/Xorg_libXext_jll.jl
+++ src/Xorg_libXext_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Xorg_libXext")
-JLLWrappers.@generate_main_file("Xorg_libXext", UUID("1082639a-0dae-5f34-9b06-72781eeb8cb3"))
+JLLWrappers.@generate_main_file("Xorg_libXext", Base.UUID("1082639a-0dae-5f34-9b06-72781eeb8cb3"))
 end  # module Xorg_libXext_jll
